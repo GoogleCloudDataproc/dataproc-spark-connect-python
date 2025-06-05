d0bc74a2627f0bc6e23fbc141a2e19a002c8efdc
Pahulpreet Singh <pahulpreets@google.com>
2025-06-05 03:51:08 +0000

    fix header
    Signed-off-by: Pahulpreet Singh <pahulpreets@google.com>

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from pyspark.sql.connect.proto import ExecutePlanRequest, UserContext
 from copy import deepcopy
 
