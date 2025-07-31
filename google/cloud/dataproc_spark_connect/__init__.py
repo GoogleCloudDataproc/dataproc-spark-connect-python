4f356e5032a272ce987728a9c6c20a1f893fb55a
fangyh20 <fangyh@google.com>
2025-07-31 11:37:22 -0700

    remove unused import

--- google/cloud/dataproc_spark_connect/__init__.py
+++ google/cloud/dataproc_spark_connect/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import importlib.metadata
-import sys
 import warnings
 
 from .session import DataprocSparkSession
